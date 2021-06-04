223b6936258fd02a91cdff8951b07531fe0ebbe7
chrisisler <christopherisler1@gmail.com>
2021-06-04 13:52:37 -0400

    On Enter, search and display weather API results
    Add vue-toasted pkg for handling error notifications upon fetching
    weather data.
    
    Wish there was a simpler way to handle the `computeDate` situation.
    
    Simple style changes to emphasize the data display.
    
    Could add `DataState` pattern like in TrainingFor to handle loading
    states in the async weather data.
    
    Vue feels like React with less thinking about little things. It's just
    figured out.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,80 @@
 <template>
   <div id="app">
-    <input type="text" placeholder="Search..." />
+    <main class="columns">
+      <input
+        type="text"
+        placeholder="Search..."
+        v-model="query"
+        v-on:keyup.enter="fetchWeather"
+      />
+      <div class="columns" v-if="typeof weather.main != 'undefined'">
+        <div class="weather-results location">
+          {{ weather.name }}, {{ weather.sys.country }}
+        </div>
+        <div class="weather-results date">
+          {{ computeDate() }}
+        </div>
+        <div class="weather-results temperature">
+          {{ Math.round(weather.main.temp) }}°f
+        </div>
+        <div class="weather-results description">
+          {{ weather.weather[0].main }}
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 export default {
   name: 'vue-weather',
   data() {
     return {
-      api_key: process.env.openWeatherMapAPIKey2VueWeather
+      urlBase: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
     };
+  },
+  methods: {
+    async fetchWeather() {
+      const url = `${this.urlBase}weather?q=${this.query}&units=imperial&appid=${process.env.VUE_APP_OPENWEATHERMAP_API_KEY}`;
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        this.weather = json;
+      } catch (error) {
+        this.$toasted.error(error.message);
+      }
+    },
+    computeDate() {
+      const now = new Date();
+      const dayName = days[now.getDay()];
+      const monthName = months[now.getMonth()];
+      return `${dayName} ${now.getDate()} ${monthName} ${now.getFullYear()}`;
+    }
   }
 };
 </script>
@@ -33,12 +97,15 @@ body {
   transition: 0.3s;
   min-height: 100vh;
   padding: 2rem;
+  text-align: center;
+}
+
+.columns {
   display: flex;
   flex-direction: column;
 }
-
-#app > *:not(:last-child) {
-  margin-bottom: 2rem;
+.columns > *:not(:last-child) {
+  margin-bottom: 1rem;
 }
 
 input {
@@ -50,7 +117,36 @@ input {
   outline: none;
   background: none;
   border-radius: 8px;
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.4);
-  background-color: #ffffff;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.3);
+  background-color: #fff;
+}
+
+.weather-results {
+  color: #fff;
+  text-shadow: 2px 2px rgba(0, 0, 0, 0.5);
+  font-size: 1.5rem;
+}
+
+.weather-results.location {
+  font-weight: 500;
+}
+
+.weather-results.date {
+  font-style: italic;
+  font-weight: 300;
+}
+
+.weather-results.temperature {
+  font-weight: 800;
+  font-size: 6rem;
+  border-radius: 20px;
+  padding: 0;
+  box-shadow: 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
+.weather-results.description {
+  font-style: italic;
+  font-weight: 500;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import Toasted from 'vue-toasted';
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -6,3 +8,9 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(Toasted, {
+  position: 'bottom-center',
+  duration: 4000,
+  singleton: true
+});
